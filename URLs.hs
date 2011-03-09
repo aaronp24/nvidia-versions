67a201988c7783d3964f78fd20ca5db257af4327
Aaron Plattner <aplattner@nvidia.com>
2011-03-08 16:00:06 -0800

    260.19.44

--- URLs.hs
+++ URLs.hs
@@ -277,6 +277,7 @@ nvidiaUrls = fromList [
     dashdv "ia32"           "260.19.21",
     dashdv "ia32"           "260.19.29",
     dashdv "ia32"           "260.19.36",
+    dashdv "ia32"           "260.19.44",
     dashdv "ia32"           "270.18",
     dashdv "ia32"           "270.26",
     nvidia "x86"            "71.86.04",
