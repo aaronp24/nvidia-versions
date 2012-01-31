295de4fe2bd2e4690a5f12e7de2f3622119beb37
Aaron Plattner <aplattner@nvidia.com>
2012-01-31 14:12:38 -0800

    295.17

--- URLs.hs
+++ URLs.hs
@@ -185,7 +185,8 @@ nvnewsUrls = fromList [
     nvnews "290.03"             2493300,
     nvnews "290.06"             2498046,
     nvnews "290.10"             2503979,
-    nvnews "295.09"             2514702
+    nvnews "295.09"             2514702,
+    nvnews "295.17"             2524716
  ]
 
 dashdv foo ver =
