a75ad4b2ba7b0732f95385f304ba256327be4897
Liam Middlebrook <lmiddlebrook@nvidia.com>
2019-06-10 12:23:53 -0700

    430.26

--- URLs.hs
+++ URLs.hs
@@ -367,7 +367,8 @@ nvnewsUrls = fromList [
     devtalk"418.56"             1048767,
     devtalk"418.74"             1051453,
     devtalk"430.09"             1050666,
-    devtalk"430.14"             1051837
+    devtalk"430.14"             1051837,
+    devtalk"430.26"             1055344
  ]
 
 geforce ver x86 amd64 =
@@ -691,6 +692,7 @@ nvidiaUrls = fromList [
     dr64                    "418.74" 146667,
     dr64                    "430.09" 146189,
     dr64                    "430.14" 147582,
+    dr64                    "430.26" 148435,
     nvidia "x86"            "71.86.04",
     nvidia "x86"            "71.86.09",
     nvidia "ia32"           "71.86.11",
