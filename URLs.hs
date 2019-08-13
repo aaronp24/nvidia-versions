a067196c9d8b73c7bb55c15662f5a71295fe1f13
Aaron Plattner <aplattner@nvidia.com>
2019-08-13 11:26:00 -0700

    435.17

--- URLs.hs
+++ URLs.hs
@@ -372,7 +372,8 @@ nvnewsUrls = fromList [
     devtalk"430.14"             1051837,
     devtalk"430.26"             1055344,
     devtalk"430.34"             1056996,
-    devtalk"430.40"             1058258
+    devtalk"430.40"             1058258,
+    devtalk"435.17"             1060977
  ]
 
 geforce ver x86 amd64 =
@@ -701,6 +702,7 @@ nvidiaUrls = fromList [
     dr64                    "430.26" 148435,
     dr64                    "430.34" 148589,
     dr64                    "430.40" 149138,
+    dr64                    "435.17" 149785,
     nvidia "x86"            "71.86.04",
     nvidia "x86"            "71.86.09",
     nvidia "ia32"           "71.86.11",
