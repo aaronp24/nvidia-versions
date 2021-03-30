a6eeae32e4ee717b4da3a660abeb5afe5fa787a5
Aaron Plattner <aplattner@nvidia.com>
2021-03-30 10:15:27 -0700

    465.19.01

--- URLs.hs
+++ URLs.hs
@@ -30,7 +30,8 @@ nvnewsUrls = fromList [
     forum       "460.32.03"         165423,
     forum       "460.39"            166842,
     forum       "460.56"            169452,
-    forum       "460.67"            172742
+    forum       "460.67"            172742,
+    forum       "465.19.01"         173745
  ]
 
 geforce ver x86 amd64 =
@@ -388,6 +389,7 @@ nvidiaUrls = fromList [
     dr64                    "460.39" 170134,
     dr64                    "460.56" 170804,
     dr64                    "460.67" 171392,
+    dr64                    "465.19.01" 171980,
     nvidia "x86"            "71.86.04",
     nvidia "x86"            "71.86.09",
     nvidia "ia32"           "71.86.11",
