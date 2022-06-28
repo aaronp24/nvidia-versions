bd485f0e885b4db2d962290c302afd544c4f737e
Aaron Plattner <aplattner@nvidia.com>
2022-06-28 10:06:07 -0700

    generate-htaccess: Use `maximum` to find the newest driver

--- generate-htaccess.hs
+++ generate-htaccess.hs
@@ -1,21 +1,11 @@
 module Main where
 import NVVersionParser
-import Data.Map(Map)
-import qualified Data.Map as Map
 
 type Key = (Branch, Maturity)
 
--- Convert a list of drivers to a (Branch, Maturity) to Version map.
-toMap :: [Driver] -> Map Key Version
-toMap versions =
-    Map.fromList (map split versions)
- where
-    split (Driver ver br mat) = ((br, mat), ver)
-
 main = do
     Right versions <- parseVersionFile
-    let verMap = toMap versions
-    let (_, newest) = Map.findMax verMap
+    let (Driver newest _ _) = maximum versions
 
     putStrLn "Redirect 302 /~aplattner/nv http://xorg.freedesktop.org/releases/individual/driver/xf86-video-nv-2.1.21.tar.bz2"
     putStrLn ("Redirect 302 /~aplattner/nvidia-readme https://download.nvidia.com/XFree86/Linux-x86_64/" ++ show newest ++ "/README")
